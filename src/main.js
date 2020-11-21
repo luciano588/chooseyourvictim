8a72dcca09bb3ed68316229cca646021f98c43cd
luciano588 <luciano588.alm@gmail.com>
2020-11-21 15:26:51 +0000

    first commit

--- src/main.js
+++ src/main.js
@@ -8,3 +8,18 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   console.log("Hello Rigo from the console!");
 };
+// function newcard() {
+//   console.log("newcard");
+
+//   let suspect = [
+//     "https://deadline.com/wp-content/uploads/2018/11/miss-scarlet.jpg",
+//     "https://i.pinimg.com/originals/57/b4/88/57b488b62e595bc5664ee819867ff085.jpg"
+//   ];
+
+//   function getRndInteger(min, max) {
+//     return Math.floor(Math.random() * (max - min)) + min;
+//   }
+
+//   document.getElementById("myImg").src =
+//     suspect[getRndInteger(0, suspect.length)];
+// }
